aafff8a7460a2b1a6322b3d78b3501040d2536d8
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2024-11-12 13:57:25 +0000

    DayNight switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+// import 'app.dart';
+import 'day_night/day_night_switch.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(const DayNightApp());
 }
