de519bdf50a173d18120de3d883275dba17d171c
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2024-11-12 14:07:52 +0000

    Google Nav Bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 // import 'app.dart';
 // import 'day_night/day_night_switch.dart';
-import 'appearance/appearance_demo.dart';
-import 'appearance/shared_preference.dart';
+// import 'appearance/appearance_demo.dart';
+// import 'appearance/shared_preference.dart';
+import 'google_nav_bar/google_nav_bar.dart';
 
 void main() async {
   // runApp(const MyApp());
   // runApp(const DayNightApp());
-  WidgetsFlutterBinding.ensureInitialized();
-  await SharedPreferencesManager.instance.init();
-  runApp(const AppearanceDemoApp());
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await SharedPreferencesManager.instance.init();
+  // runApp(const AppearanceDemoApp());
+  runApp(const GoogleNavApp());
 }
